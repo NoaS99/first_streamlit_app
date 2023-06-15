6b134c8ead0a1f89f6d39a04acbad37fa25ab932
NoaS99 <136706100+NoaS99@users.noreply.github.com>
2023-06-15 15:24:40 +0300

    Update streamlit_app.py
    replaced first item emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Moms New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🍲 'Omega 3 & Blueberry Oatmeal')
 streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
 streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
 streamlit.text(🥑🍞 'Avocado Toast')
