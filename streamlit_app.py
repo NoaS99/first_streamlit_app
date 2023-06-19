fb8e80c9342ce7986bbd53f8936570042d87aeac
NoaS99 <136706100+NoaS99@users.noreply.github.com>
2023-06-19 09:41:42 +0300

    Update streamlit_app.py
    Adding Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
