995ebd2001dc55f2f765bf81d8188fee1ed8bd6b
NoaS99 <136706100+NoaS99@users.noreply.github.com>
2023-06-19 09:13:34 +0300

    Update streamlit_app.py
    Removed leading apostrophe in a few of the breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ streamlit.title('My Moms New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 'Avocado Toast')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
