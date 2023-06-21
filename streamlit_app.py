9f9fe83fbbda9a71c660c509ccd1d5546036cd93
NoaS99 <136706100+NoaS99@users.noreply.github.com>
2023-06-21 15:55:05 +0300

    Update streamlit_app.py
    removed a row that created an error

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
